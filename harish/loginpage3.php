5db22eda30b09b6cb2cb9a83edb9e1c1d2b62960
harisakthi2001 <112241010+harisakthi2001@users.noreply.github.com>
2022-11-03 09:44:52 +0530

    Add files via upload

---
+++ harish/loginpage3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>WELCOME PAGE</title>
+	</head>
+	<body>
+		<center>
+			<img src="guvilogo.png" style="width: 200px">
+		<div class="card mt-5" style="width: 60rem" align="center">
+			<h1 class="center mt-3	">WELCOME TO GUVI</h1>
+			<h2 class="center mt-3">Grab Your Vernacular Imprint</h2>
+			<h4 class="center mt-4">It Is An Online Platform To Learning The Programming Languages Like Python,Java,C,C++,Csharp,Boostrap,etc.....</h4>
+			<div class="center sm-3">
+			<button type="button" class="btn btn-primary ">START COURSE	
+			</button>
+			</div>
+
+			
+		</div>
+		</center>
+	
+	</body>
+	</html>	
