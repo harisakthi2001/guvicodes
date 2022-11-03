12ba6554d10dc00dbf59d3755d938cc6ec7a10e8
harisakthi2001 <112241010+harisakthi2001@users.noreply.github.com>
2022-11-03 09:45:15 +0530

    Add files via upload

---
+++ harish/php.php
@@ -0,0 +1,14 @@
+	<?php
+	if (isset($_POST['create'])) {
+	
+		$firstName = $_POST['firstName'];
+		$lastName = $_POST['lastName'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		echo "submitted;
+			
+			
+				
+	}
+	?>
