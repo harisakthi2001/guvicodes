39ec6abd8bbdcac3a0677091dd31b9d128d56da7
harisakthi2001 <harisakthi9255@gmail.com>
2025-08-07 10:28:25 +0530

    pushing the data to github

---
+++ muthayammal/src/main/java/gitconnection/harish.java
@@ -0,0 +1,18 @@
+package gitconnection;
+
+import java.util.Scanner;
+
+public class harish {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),f=1;
+		for(int i=1;i<=n;i++)
+		{
+			f=f*i;
+		}
+		System.out.println(f);
+
+	}
+
+}
